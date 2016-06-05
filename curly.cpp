a4a69a4a8c650a337d3bd8f34972e2d6678d1708
Rodrigo Rojo <rrojo@RodrigosMacBook.home>
2016-06-05 01:22:34 -0700

    added support to xml and json content types

--- curly.cpp
+++ curly.cpp
@@ -12,9 +12,10 @@ int main(int argc, char* argv[])
         HttpResponse response = client.Get(argv[1]);
         auto content_type = response.Header("Content-Type");
         if (content_type.find("text/") != string::npos ||
-            content_type == "application/json")
+            content_type == "application/json" ||
+            content_type == "application/xml")
         {
-            cout << response.GetBody();
+            cout << response.GetBody() << endl;
         }
         
     }
