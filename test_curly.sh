b7fc295d04f6d31efbacc812b3b32e8f949a17ae
Rodrigo Rojo <rrojo@RodrigosMacBook.home>
2016-06-05 01:23:26 -0700

    added tests for json/xml

--- test_curly.sh
+++ test_curly.sh
@@ -2,4 +2,21 @@
 if [ ! -f ./curly ]; then
     g++ curly.cpp url.cpp client.cpp request.cpp response.cpp  -o curly -g3 -std=c++11
 fi
-./curly $@
+if [ $# -eq 0 ]; then
+    echo "***********  http://www.httpbin.org/html **************"
+    ./curly http://www.httpbin.org/html
+    echo "***********  http://www.httpbin.org/deny **************"
+    ./curly http://www.httpbin.org/deny
+    echo "***********  http://www.httpbin.org/ip **************"
+    ./curly http://www.httpbin.org/ip
+    echo "***********  http://www.httpbin.org/user-agent **************"
+    ./curly http://www.httpbin.org/user-agent
+    echo "***********  http://www.httpbin.org/headers **************"
+    ./curly http://www.httpbin.org/headers
+    echo "***********  http://www.httpbin.org/get **************"
+    ./curly http://www.httpbin.org/get
+    echo "***********  http://www.httpbin.org/xml **************"
+    ./curly http://www.httpbin.org/xml
+else
+    ./curly $@
+fi
