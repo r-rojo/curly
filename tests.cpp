4476b3ffed471f6ae0ce0ca34ccc80f709bf70b1
Rodrigo Rojo <rodrigo.rojo@intel.com>
2017-12-18 18:45:41 -0800

    Add parameterized tests

--- tests.cpp
+++ tests.cpp
@@ -1,12 +1,20 @@
 #include "gtest/gtest.h"
 #include "client.h"
 
-TEST(client, get_html) {
-  http_client c;
-  auto r = c.get("http://www.httpbin.org/html");
-  EXPECT_EQ(200, r.status_code());
 
+class GetTest : public ::testing::TestWithParam<const char*>{
+
+};
+
+TEST_P(GetTest, client_get) {
+  http_client c;
+  auto r = c.get(GetParam());
+  EXPECT_EQ(200, r.status_code());
 }
+
+INSTANTIATE_TEST_CASE_P(GetTest, GetTest,
+    ::testing::Values("http://www.httpbin.org/html",
+                      "http://www.httpbin.org/deny"));
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
